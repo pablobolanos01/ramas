dc67b26b970e71ecb802469cc45f0d2000b87ed3
pablobolanos01 <pablobolanosgallego@gmail.com>
2021-02-08 09:44:25 +0100

    inicio

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <form name="formulario" action="procesar.php" method="POST">
+        	Introduce tu nombre: <input type="text" name="nombre">
+        	Introduce la edad: <input type="text" name="edad">
+        	<input type="submit" value="Enviar">
+        </form>
+    </body>
+</html>
